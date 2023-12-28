5be2c0997b7cd22e85a53b78497b79b795958fec
Azis202017 <azisa6980@gmail.com>
2023-12-29 04:04:43 +0700

    [Font] add bold, medium, semi bold, regular into paragraph text style

--- lib/app/shared/themes/font.dart
+++ lib/app/shared/themes/font.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'color.dart';
 
 TextStyle t1 = GoogleFonts.manrope(
   fontSize: 72,
@@ -43,25 +42,96 @@ TextStyle t8 = GoogleFonts.manrope(
 
 TextStyle p1 = GoogleFonts.manrope(
   fontSize: 20,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w400,
 );
 TextStyle p2 = GoogleFonts.manrope(
   fontSize: 18,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w400,
 );
 TextStyle p3 = GoogleFonts.manrope(
   fontSize: 16,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w400,
 );
 TextStyle p4 = GoogleFonts.manrope(
   fontSize: 14,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w400,
 );
 
 TextStyle p5 = GoogleFonts.manrope(
   fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
+
+
+
+TextStyle p1Medium = GoogleFonts.manrope(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+TextStyle p2Medium = GoogleFonts.manrope(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+TextStyle p3Medium = GoogleFonts.manrope(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+TextStyle p4Medium = GoogleFonts.manrope(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle p5Medium = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle p1semiBold = GoogleFonts.manrope(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+);
+TextStyle p2semiBold = GoogleFonts.manrope(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+TextStyle p3semiBold = GoogleFonts.manrope(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+TextStyle p4semiBold = GoogleFonts.manrope(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle p5semiBold = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+);
+
+
+TextStyle p1Bold = GoogleFonts.manrope(
+  fontSize: 20,
   fontWeight: FontWeight.w700,
 );
+TextStyle p2Bold = GoogleFonts.manrope(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+);
+TextStyle p3Bold = GoogleFonts.manrope(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+TextStyle p4Bold = GoogleFonts.manrope(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle p5Bold = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+);
+
+
 
 TextStyle u1 = GoogleFonts.manrope(
   fontSize: 12,
