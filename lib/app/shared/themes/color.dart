8475cbc3d84fbd34f09db50fd884d7aa6fe256b2
Azis202017 <azisa6980@gmail.com>
2023-12-29 04:01:55 +0700

    Font and color theme

---
+++ lib/app/shared/themes/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color primaryBetterColor = Color(0xff1872F6);
+Color primaryHighlighterColor = Color(0xff1C293E);
---
+++ lib/app/shared/themes/font.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'color.dart';
+
+TextStyle t1 = GoogleFonts.manrope(
+  fontSize: 72,
+  fontWeight: FontWeight.w700,
+);
+TextStyle t2 = GoogleFonts.manrope(
+  fontSize: 66,
+  fontWeight: FontWeight.w700,
+);
+TextStyle t3 = GoogleFonts.manrope(
+  fontSize: 56,
+  fontWeight: FontWeight.w700,
+);
+TextStyle t4 = GoogleFonts.manrope(
+  fontSize: 48,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle t5 = GoogleFonts.manrope(
+  fontSize: 40,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle t6 = GoogleFonts.manrope(
+  fontSize: 36,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle t7 = GoogleFonts.manrope(
+  fontSize: 28,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle t8 = GoogleFonts.manrope(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+
+TextStyle p1 = GoogleFonts.manrope(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+);
+TextStyle p2 = GoogleFonts.manrope(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+);
+TextStyle p3 = GoogleFonts.manrope(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+TextStyle p4 = GoogleFonts.manrope(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle p5 = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle u1 = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.underline,
+);
+
+TextStyle u2 = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.underline,
+
+);
+
+TextStyle u3 = GoogleFonts.manrope(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.underline,
+
+);
+
