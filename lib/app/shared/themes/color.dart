80864ec4ed222a715360100d05c05840d5ac36f2
Azis202017 <azisa6980@gmail.com>
2023-12-29 04:17:01 +0700

    [Theme] added color for application

--- lib/app/shared/themes/color.dart
+++ lib/app/shared/themes/color.dart
@@ -1,4 +1,32 @@
 import 'package:flutter/material.dart';
 
-Color primaryBetterColor = Color(0xff1872F6);
-Color primaryHighlighterColor = Color(0xff1C293E);
+Color primaryBetterColor = const Color(0xff1872F6);
+Color primaryEbonyClayColor = const Color(0xff1C293E);
+
+Color secondarySteelColor = const Color(0xff75869C);
+Color secondaryLovenderMistColor = const Color(0xffE3E9F0);
+Color secondaryWhiteColor = Colors.white;
+
+
+Color grey900Color = const Color(0xff161F42);
+Color grey800Color = const Color(0xff545C7C);
+Color grey700Color = const Color(0xff767B92);
+Color grey600Color = const Color(0xff999DAE);
+Color grey500Color = const Color(0xffB7BAC9);
+Color grey400Color = const Color(0xffD2D5DF);
+Color grey300Color = const Color(0xffE3E5ED);
+Color grey200Color = const Color(0xffEFF1F8);
+Color grey100Color = const Color(0xffF8F9FD);
+
+Color otherLeofyGreenColor = const Color(0xff41B631);
+Color otherPurpleMimosaColor = const Color(0xffAC7DF5);
+Color otherCodmiumRedColor = const Color(0xffE1102B);
+Color otherMacowBlueGreenColor = const Color(0xff26C1CA);
+Color otherPumpkinOrangeColor = const Color(0xffFE7B1E);
+Color otherSuperNovaColor = const Color(0xffFBCA00);
+Color otherLightTealColor = const Color(0xff9EE0B7);
+Color otherHarpColor = const Color(0xffE7F2E7);
+Color otherCasalColor = const Color(0xff2E5E68);
+Color otherLightGreyColor = const Color(0xffD9D9D9);
+Color otherParcelalnColor = const Color(0xffF2F2F2);
+Color otherCosmosColor = const Color(0xffF8D9D6);
