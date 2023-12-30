787160a3a45b435553f9ad083a7ca74c25fd7af2
Azis202017 <azisa6980@gmail.com>
2023-12-31 05:35:59 +0700

    [Welcome] fix bug

--- lib/app/modules/welcome_before_login/controllers/welcome_before_login_controller.dart
+++ lib/app/modules/welcome_before_login/controllers/welcome_before_login_controller.dart
@@ -4,21 +4,37 @@ import '../../../constant/global_variable.dart';
 import '../../../routes/app_pages.dart';
 
 class WelcomeBeforeLoginController extends GetxController {
+  bool isLoading = true;
+
   @override
   void onReady() {
     super.onReady();
     checkIsFirstTime();
   }
 
-  void toHomePage() {
+  void toHomePage() async {
+    isLoading = true; 
+
+    await Future.delayed(const Duration(seconds: 2));
+
     storage.write('welcome_before_login', 1);
+    isLoading = false; 
+    update();
     Get.offAllNamed(Routes.LOGIN);
   }
 
+  void checkIsFirstTime() async {
+    isLoading = true; 
 
-  void checkIsFirstTime() {
+    await Future.delayed(const Duration(seconds: 3));
+
     if (storage.read('welcome_before_login') == 1) {
+      isLoading = false; 
       Get.offAllNamed(Routes.LOGIN);
+    } else {
+      isLoading = false; 
+      
     }
+    update();
   }
 }
--- lib/app/modules/welcome_before_login/views/welcome_before_login_view.dart
+++ lib/app/modules/welcome_before_login/views/welcome_before_login_view.dart
@@ -1,4 +1,5 @@
 import 'package:better_app/app/shared/themes/color.dart';
+import 'package:better_app/app/shared/widgets/center_loading.dart';
 import 'package:better_app/app/shared/widgets/primary_button.dart';
 import 'package:better_app/app/shared/widgets/subtitle_text.dart';
 import 'package:flutter/material.dart';
@@ -12,80 +13,84 @@ class WelcomeBeforeLoginView extends GetView<WelcomeBeforeLoginController> {
   const WelcomeBeforeLoginView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                PrimaryButton(
-                  title: 'Skip',
-                  onPressed: controller.toHomePage,
-                  width: 82,
-                  height: 56,
-                  borderRadius: 8,
-                  backgroundColor: grey100Color,
-                  foregroundColor: primaryEbonyClayColor,
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                    right: 86,
-                  ),
-                  child: RichFourText(
-                    text1: 'Learning becomes',
-                    text2: ' fun',
-                    text3: ' and',
-                    text4: ' easy',
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                const SubtitleText(
-                  subtitle:
-                      'Log in to personalize your experience & track your progress',
-                ),
-                
-              ],
-            ),
-          ),
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 190,
-          child: Column(
-            children: [
-              const Center(
-                child: SubtitleText(
-                  subtitle:
-                      'By continuing I agree with the Terms & Conditions, Privacy Policy',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
+    return GetBuilder<WelcomeBeforeLoginController>(
+      builder: (_) {
+        return controller.isLoading ? const CenterLoading(): SafeArea(
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Container(
+                margin: const EdgeInsets.symmetric(
                   horizontal: 20,
+                  vertical: 40,
                 ),
-                child: PrimaryButton(
-                  title: 'Continue to login',
-                  backgroundColor: primaryEbonyClayColor,
-                  onPressed: controller.toHomePage,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    PrimaryButton(
+                      title: 'Skip',
+                      onPressed: controller.toHomePage,
+                      width: 82,
+                      height: 56,
+                      borderRadius: 8,
+                      backgroundColor: grey100Color,
+                      foregroundColor: primaryEbonyClayColor,
+                    ),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        right: 86,
+                      ),
+                      child: RichFourText(
+                        text1: 'Learning becomes',
+                        text2: ' fun',
+                        text3: ' and',
+                        text4: ' easy',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const SubtitleText(
+                      subtitle:
+                          'Log in to personalize your experience & track your progress',
+                    ),
+                    
+                  ],
                 ),
               ),
-            ],
+            ),
+            bottomNavigationBar: SizedBox(
+              height: 190,
+              child: Column(
+                children: [
+                  const Center(
+                    child: SubtitleText(
+                      subtitle:
+                          'By continuing I agree with the Terms & Conditions, Privacy Policy',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: PrimaryButton(
+                      title: 'Continue to login',
+                      backgroundColor: primaryEbonyClayColor,
+                      onPressed: controller.toHomePage,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
